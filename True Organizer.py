1bc7999c90f795f6c84f3e6be8ffaa7f1083a86a
RoshanDileep <118088610+RoshanDileep@users.noreply.github.com>
2023-05-06 16:37:11 -0700

    Change folder browsing system

--- True Organizer.py
+++ True Organizer.py
@@ -3,7 +3,7 @@
 # # import os
  
 # # # Get the list of all files and directories
-source_folder = r"C:\Users\rosha\OneDrive\Documents\GitHub\File-Organizer\Organize Files" + '\\'
+# source_folder = r"C:\Users\rosha\OneDrive\Documents\GitHub\File-Organizer\Organize Files" + '\\'
 music_folder = r"C:\Users\rosha\OneDrive\Documents\GitHub\File-Organizer\Organize Files\Music" + '\\'
 pictures_folder = r"C:\Users\rosha\OneDrive\Documents\GitHub\File-Organizer\Organize Files\Pictures" + '\\'
 videos_folder = r"C:\Users\rosha\OneDrive\Documents\GitHub\File-Organizer\Organize Files\Videos" + '\\'
@@ -56,27 +56,36 @@ class FileOrganizerApp(QWidget):
         self.layout.addWidget(self.label)
         self.layout.addWidget(self.button)
         self.setLayout(self.layout)
-
     def organize_files(self):
         source_folder = QFileDialog.getExistingDirectory(self, "Select Source Folder")
         if source_folder:
             target_folder = os.path.join(source_folder, "Organized Files")
             os.makedirs(target_folder, exist_ok=True)
+            music_folder = os.path.join(target_folder, "Music")
+            pictures_folder = os.path.join(target_folder, "Pictures")
+            videos_folder = os.path.join(target_folder, "Videos")
+            os.makedirs(music_folder, exist_ok=True)
+            os.makedirs(pictures_folder, exist_ok=True)
+            os.makedirs(videos_folder, exist_ok=True)
+
             for path, dir, files in os.walk(source_folder):
                 if files:
                     for file in files:
-                        if file.endswith((".mp3",  ".wav", ".aac", )):
-                            if not os.path.isfile(music_folder + file):
-                                os.rename(path + '\\' + file, music_folder + file)
-                        if file.endswith((".mp4", ".mov", ".wmv", ".avi", ".avchd", ".flv", ".f4v", ".swf", ".mkv", ".webm")):
-                            if not os.path.isfile(videos_folder + file):
-                                os.rename(path + '\\' + file, videos_folder + file)
-                        if file.endswith((".jpeg", ".jpg", ".png", ".gif", ".tiff", ".bmp", ".raw")):
-                            if not os.path.isfile(pictures_folder + file):
-                                os.rename(path + '\\' + file, pictures_folder + file)
-            
-            
+                        if file.endswith((".mp3",  ".wav", ".aac")):
+                            if not os.path.isfile(os.path.join(music_folder, file)):
+                                os.rename(os.path.join(path, file), os.path.join(music_folder, file))
+                        elif file.endswith((".jpeg", ".jpg", ".png", ".gif", ".tiff", ".bmp", ".raw")):
+                            if not os.path.isfile(os.path.join(pictures_folder, file)):
+                                os.rename(os.path.join(path, file), os.path.join(pictures_folder, file))
+                        elif file.endswith((".mp4", ".mov", ".wmv", ".avi", ".avchd", ".flv", ".f4v", ".swf", ".mkv", ".webm")):
+                            if not os.path.isfile(os.path.join(videos_folder, file)):
+                                os.rename(os.path.join(path, file), os.path.join(videos_folder, file))
+
             self.label.setText("Files organized successfully!")
+
+                
+                
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
